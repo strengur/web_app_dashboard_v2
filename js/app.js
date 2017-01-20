3b0e711e949d1efa24ecfb6daf13a1da639cd260
Steingrímur <steingrimur@740.is>
2017-01-20 23:47:16 +0000

    Other charts begin to take shapes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var $closeAlert = document.getElementById('alert-bar-close');
-var $webTraffic = document.getElementById('web-traffic').getContext("2d");
+var $webTraffic = document.getElementById('web-traffic').getContext('2d');
+var $dailyTraffic = document.getElementById('daily-traffic').getContext('2d');
+var $mobileUsers = document.getElementById('mobile-users').getContext('2d');
 
 $closeAlert.addEventListener('click', function() {
   $(this).parent().fadeOut();
@@ -11,7 +13,6 @@ var $webTrafficChart = new Chart($webTraffic, {
     type: 'line',
     data: {
       labels: ["16-22", "23-29", "30-36", "37-43", "44-50", "51-57", "58-64"],
-
       datasets: [{
         //label: 'Web Traffic',
         fill: true,
@@ -47,7 +48,6 @@ var $webTrafficChart = new Chart($webTraffic, {
           display: true,
           ticks: {
             beginAtZero: true,
-            autoSkipPadding: 1
           }
         }],
         yAxes: [{
@@ -61,4 +61,79 @@ var $webTrafficChart = new Chart($webTraffic, {
         }]
       }
     }
+});
+
+var $dailyTrafficChart = new Chart($dailyTraffic, {
+  type: 'bar',
+  data: {
+    labels: ['Monday', 'Thuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    datasets: [
+      {
+        label: "Daily Traffic",
+        backgroundColor: [
+          'rgba(252, 64, 23, 0.4)',
+          'rgba(25, 164, 123, 0.4)',
+          'rgba(12, 4, 223, 0.4)',
+          'rgba(22, 224, 243, 0.4)',
+          'rgba(111, 33, 189, 0.4)',
+          'rgba(252, 64, 87, 0.4)',
+          'rgba(1, 76, 23, 0.4)'
+        ],
+        borderColor: [
+          'rgba(252, 64, 23, 1)',
+          'rgba(25, 164, 123, 1)',
+          'rgba(12, 4, 223, 1)',
+          'rgba(22, 224, 243, 1)',
+          'rgba(111, 33, 189, 1)',
+          'rgba(252, 64, 87, 1)',
+          'rgba(1, 76, 23, 1)'
+        ],
+        hoverBackgroundColor: [
+          'rgba(252, 64, 23, 0.6)',
+          'rgba(25, 164, 123, 0.6)',
+          'rgba(12, 4, 223, 0.6)',
+          'rgba(22, 224, 243, 0.6)',
+          'rgba(111, 33, 189, 0.6)',
+          'rgba(252, 64, 87, 0.6)',
+          'rgba(1, 76, 23, 0.6)'
+        ],
+        borderWidth: 2,
+        data: [226, 283, 234, 237, 223, 254, 264]
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    layout: {
+      padding: 30
+    },
+  }
+});
+
+var $mobileUsersChart = new Chart($mobileUsers, {
+  type: 'pie',
+  data: {
+    labels: [
+      'Mobile',
+      'Desktop',
+      'Tablet'
+    ],
+    datasets: [
+      {
+        data: [457, 542, 384],
+        backgroundColor: [
+          '#2f45f3',
+          '#ff53cd',
+          '#cd8dc8'
+        ]
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    cutoutPercentage: 50,
+    layout: {
+      padding: 30
+    }
+  }
 });
