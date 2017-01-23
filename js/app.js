03e33915e9929c46a9619942213ce807a7980a5a
Steingrímur <steingrimur@740.is>
2017-01-23 17:31:04 +0000

    changes for calc in CSS

--- js/app.js
+++ js/app.js
@@ -26,13 +26,16 @@ var $webTrafficChart = new Chart($webTraffic, {
         pointHoverBackgroundColor: "rgba(75,192,192,1)",
         pointHoverBorderColor: "rgba(220,220,220,1)",
         pointHoverBorderWidth: 2,
-        pointRadius: 10,
-        pointHitRadius: 10,
-        data: [2003, 1344, 3458, 3524, 2647, 3633, 1488]
+        pointRadius: 5,
+        pointHitRadius: 5,
+        data: [1003, 344, 2458, 2524, 1647, 2633, 488]
       }]
     },
     options: {
       responsive: true,
+      legend: {
+        display: false
+      },
       layout: {
         padding: 30
       },
